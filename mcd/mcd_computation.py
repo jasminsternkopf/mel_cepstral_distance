fb467dfab36fe5b25ed4f60608a85545d24f5696
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-03-03 16:48:27 +0100

    .

--- mcd/mcd_computation.py
+++ mcd/mcd_computation.py
@@ -1,38 +1,76 @@
+from typing import Tuple
+
 import librosa
 import numpy as np
 from fastdtw.fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 
-from mcd.parameters import *
+
+def get_mcd_from_paths(path_1: str, path_2: str, n_fft: int = 1024, hop_length: int = 256, n_mels: int = 20, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
+  audio_1, sr_1 = get_audio_from_path(path_1)
+  audio_2, sr_2 = get_audio_from_path(path_2)
+  spectogram_1 = get_spectogram(audio_1, n_fft, hop_length)
+  spectogram_2 = get_spectogram(audio_2, n_fft, hop_length)
+  mel_spectogram_1 = get_mel_spectogram(spectogram_1, sr_1, n_mels)
+  mel_spectogram_2 = get_mel_spectogram(spectogram_2, sr_2, n_mels)
+  return get_mcd_from_mel_spectograms(mel_spectogram_1, mel_spectogram_2, no_of_coeffs_per_frame)
 
 
-def get_mel_cepstral_distance(input_path: str, output_path: str, N=None):
-  input_as_array, _ = librosa.load(input_path, mono=True)
-  output_as_array, _ = librosa.load(output_path, mono=True)
-  mcd, no_of_frames = mel_cepstral_dist_faster(input_as_array, output_as_array)
-  print(
-    f"The mel cepstral distance between the given audios is {mcd} (Number of frames: {no_of_frames})")
+def get_mcd_from_mel_spectograms(mel_spectogram_1: np.ndarray, mel_spectogram_2: np.ndarray, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
+  mfccs_1 = get_mfccs(mel_spectogram_1, no_of_coeffs_per_frame)
+  mfccs_2 = get_mfccs(mel_spectogram_2, no_of_coeffs_per_frame)
+  return mel_cepstral_dist_dtw(mfccs_1, mfccs_2)
 
 
-def get_mfccs(loaded_audio: np.array):
-  audio_stft = librosa.stft(loaded_audio, n_fft=FRAME_SIZE, hop_length=HOP_SIZE, center=False)
-  spectogram = np.abs(audio_stft) ** 2
-  mel_spectogram = MEL_FILTER_BANK @ spectogram
+def get_audio_from_path(path: str):
+  audio, sr = librosa.load(path, mono=True)
+  return audio, sr
+
+
+def get_spectogram(audio: np.ndarray, n_fft: int = 1024, hop_length: int = 256) -> np.ndarray:
+  stft_of_audio = librosa.stft(audio, n_fft=n_fft, hop_length=hop_length, center=False)
+  spectogram = np.abs(stft_of_audio) ** 2
+  return spectogram
+
+
+def get_mel_spectogram(spectogram: np.ndarray, sr: int = 22050, n_mels: int = 20) -> np.ndarray:
+  n_fft = (spectogram.shape[0] - 1) * 2
+  mel_filter_bank = librosa.filters.mel(
+    sr=sr, n_fft=n_fft, n_mels=n_mels, norm=None, dtype=np.float64, htk=True)
+  mel_spectogram = mel_filter_bank @ spectogram
+  return mel_spectogram
+
+
+def cos_func(i, n):
+  return np.cos((i + 1) * (n + 1 / 2) * np.pi / 20)
+
+
+def get_mfccs(mel_spectogram: np.ndarray, no_of_coeffs_per_frame: int = 16) -> np.ndarray:
   log_mel_spectogram = np.log10(mel_spectogram)
-  mfccs = COS_MATRIX @ log_mel_spectogram
+  n_mels = mel_spectogram.shape[0]
+  cos_matrix = np.fromfunction(cos_func, (n_mels, no_of_coeffs_per_frame), dtype=np.float64)
+  mfccs = cos_matrix @ log_mel_spectogram
   return mfccs
 
 
-def mel_cepstral_dist_faster(input_audio: np.array, output_audio: np.array):
-  mfccs_input = get_mfccs(input_audio).T
-  mfccs_output = get_mfccs(output_audio).T
-  _, path_between_mfccs = fastdtw(mfccs_input, mfccs_output, dist=euclidean)
-  path_for_input = list(map(lambda l: l[0], path_between_mfccs))
-  path_for_output = list(map(lambda l: l[1], path_between_mfccs))
-  mfccs_input = mfccs_input[path_for_input]
-  mfccs_output = mfccs_output[path_for_output]
-  mfccs_diff = mfccs_output - mfccs_input
+def mel_cepstral_dist_dtw(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, int]:
+  aligned_mfccs_1, aligned_mfccs_2 = align_mfccs_with_dtw(mfccs_1, mfccs_2)
+  return mel_cepstral_dist(aligned_mfccs_1, aligned_mfccs_2)
+
+
+def mel_cepstral_dist(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, int]:
+  assert mfccs_1.shape == mfccs_2.shape
+  mfccs_diff = mfccs_1 - mfccs_2
   mfccs_diff_norms = np.linalg.norm(mfccs_diff, axis=1)
   mcd = np.mean(mfccs_diff_norms)
-  frames = len(mfccs_diff_norms)
-  return mcd, frames
+  no_of_frames = len(mfccs_diff_norms)
+  return mcd, no_of_frames
+
+
+def align_mfccs_with_dtw(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+  _, path_between_mfccs = fastdtw(mfccs_1, mfccs_2, dist=euclidean)
+  path_for_input = list(map(lambda l: l[0], path_between_mfccs))
+  path_for_output = list(map(lambda l: l[1], path_between_mfccs))
+  mfccs_1 = mfccs_1[path_for_input]
+  mfccs_2 = mfccs_2[path_for_output]
+  return mfccs_1, mfccs_2

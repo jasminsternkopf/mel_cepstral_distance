2779b3ea36d37bbc1ebd32741a2aec104d781a9f
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-03-03 17:30:46 +0100

    adds init, small changes

---
+++ mcd/__init__.py
@@ -0,0 +1,5 @@
+from mcd.mcd_computation import (get_audio_from_path,
+                                 get_mcd_dtw_from_mel_spectograms,
+                                 get_mcd_dtw_from_paths, get_mel_spectogram,
+                                 get_mfccs, get_spectogram, mel_cepstral_dist,
+                                 mel_cepstral_dist_dtw)
--- mcd/mcd_computation.py
+++ mcd/mcd_computation.py
@@ -6,23 +6,23 @@ from fastdtw.fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 
 
-def get_mcd_from_paths(path_1: str, path_2: str, n_fft: int = 1024, hop_length: int = 256, n_mels: int = 20, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
+def get_mcd_dtw_from_paths(path_1: str, path_2: str, n_fft: int = 1024, hop_length: int = 256, n_mels: int = 20, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
   audio_1, sr_1 = get_audio_from_path(path_1)
   audio_2, sr_2 = get_audio_from_path(path_2)
   spectogram_1 = get_spectogram(audio_1, n_fft, hop_length)
   spectogram_2 = get_spectogram(audio_2, n_fft, hop_length)
   mel_spectogram_1 = get_mel_spectogram(spectogram_1, sr_1, n_mels)
   mel_spectogram_2 = get_mel_spectogram(spectogram_2, sr_2, n_mels)
-  return get_mcd_from_mel_spectograms(mel_spectogram_1, mel_spectogram_2, no_of_coeffs_per_frame)
+  return get_mcd_dtw_from_mel_spectograms(mel_spectogram_1, mel_spectogram_2, no_of_coeffs_per_frame)
 
 
-def get_mcd_from_mel_spectograms(mel_spectogram_1: np.ndarray, mel_spectogram_2: np.ndarray, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
+def get_mcd_dtw_from_mel_spectograms(mel_spectogram_1: np.ndarray, mel_spectogram_2: np.ndarray, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
   mfccs_1 = get_mfccs(mel_spectogram_1, no_of_coeffs_per_frame)
   mfccs_2 = get_mfccs(mel_spectogram_2, no_of_coeffs_per_frame)
   return mel_cepstral_dist_dtw(mfccs_1, mfccs_2)
 
 
-def get_audio_from_path(path: str):
+def get_audio_from_path(path: str) -> Tuple[np.ndarray, int]:
   audio, sr = librosa.load(path, mono=True)
   return audio, sr
 
@@ -41,7 +41,7 @@ def get_mel_spectogram(spectogram: np.ndarray, sr: int = 22050, n_mels: int = 20
   return mel_spectogram
 
 
-def cos_func(i, n):
+def cos_func(i: int, n: int) -> float:
   return np.cos((i + 1) * (n + 1 / 2) * np.pi / 20)
 
 
@@ -59,7 +59,11 @@ def mel_cepstral_dist_dtw(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[flo
 
 
 def mel_cepstral_dist(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, int]:
-  assert mfccs_1.shape == mfccs_2.shape
+  if mfccs_1.shape[0] != mfccs_2.shape[0]:
+    raise Exception("The number of coefficients per frame has to be the same for both inputs.")
+  if mfccs_1.shape[1] != mfccs_2.shape[1]:
+    raise Exception(
+      "The number of frames has to be the same for both inputs. Please use mel_cepstral_dist_dtw.")
   mfccs_diff = mfccs_1 - mfccs_2
   mfccs_diff_norms = np.linalg.norm(mfccs_diff, axis=1)
   mcd = np.mean(mfccs_diff_norms)

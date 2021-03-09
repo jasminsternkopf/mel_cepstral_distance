dc0e235b3c82c87462c2a8705608f8c3049ea578
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-03-09 22:22:18 +0100

    adds possib. to align mfccs by filling with zeros

--- mcd/mcd_computation.py
+++ mcd/mcd_computation.py
@@ -16,6 +16,16 @@ def get_mcd_dtw_from_paths(path_1: str, path_2: str, n_fft: int = 1024, hop_leng
   return get_mcd_dtw_from_mel_spectograms(mel_spectogram_1, mel_spectogram_2, no_of_coeffs_per_frame)
 
 
+def get_mcd_fill_with_zeros_from_paths(path_1: str, path_2: str, n_fft: int = 1024, hop_length: int = 256, n_mels: int = 20, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
+  audio_1, sr_1 = get_audio_and_sampling_rate_from_path(path_1)
+  audio_2, sr_2 = get_audio_and_sampling_rate_from_path(path_2)
+  spectogram_1 = get_spectogram(audio_1, n_fft, hop_length)
+  spectogram_2 = get_spectogram(audio_2, n_fft, hop_length)
+  mel_spectogram_1 = get_mel_spectogram(spectogram_1, sr_1, n_mels)
+  mel_spectogram_2 = get_mel_spectogram(spectogram_2, sr_2, n_mels)
+  return mel_cepstral_dist_fill_with_zeros(mel_spectogram_1, mel_spectogram_2)
+
+
 def get_mcd_dtw_from_mel_spectograms(mel_spectogram_1: np.ndarray, mel_spectogram_2: np.ndarray, no_of_coeffs_per_frame: int = 16) -> Tuple[float, int]:
   mfccs_1 = get_mfccs(mel_spectogram_1, no_of_coeffs_per_frame)
   mfccs_2 = get_mfccs(mel_spectogram_2, no_of_coeffs_per_frame)
@@ -62,6 +72,27 @@ def mel_cepstral_dist_dtw(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[flo
   return mel_cepstral_dist(aligned_mfccs_1, aligned_mfccs_2)
 
 
+def mel_cepstral_dist_fill_with_zeros(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, int]:
+  if mfccs_1.shape[0] != mfccs_2.shape[0]:
+    raise Exception("The number of coefficients per frame has to be the same for both inputs.")
+  aligned_mfccs_1, aligned_mfccs_2 = fill_rest_with_zeros(mfccs_1, mfccs_2)
+  return mel_cepstral_dist(aligned_mfccs_1, aligned_mfccs_2)
+
+
+def fill_rest_with_zeros(array_1: np.ndarray, array_2: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+  dim_1 = array_1.shape[1]
+  dim_2 = array_2.shape[1]
+  diff = abs(dim_1 - dim_2)
+  if diff > 0:
+    adding_array = np.zeros(shape=(array_1.shape[0], diff))
+    if dim_1 < dim_2:
+      array_1 = np.concatenate((array_1, adding_array), axis=1)
+    else:
+      array_2 = np.concatenate((array_2, adding_array), axis=1)
+  assert array_1.shape == array_2.shape
+  return array_1, array_2
+
+
 def mel_cepstral_dist(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, int]:
   if mfccs_1.shape[0] != mfccs_2.shape[0]:
     raise Exception("The number of coefficients per frame has to be the same for both inputs.")
@@ -69,7 +100,7 @@ def mel_cepstral_dist(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[float, 
     raise Exception(
       "The number of frames has to be the same for both inputs. Please use mel_cepstral_dist_dtw.")
   mfccs_diff = mfccs_1 - mfccs_2
-  mfccs_diff_norms = np.linalg.norm(mfccs_diff, axis=1)
+  mfccs_diff_norms = np.linalg.norm(mfccs_diff, axis=0)
   mcd = np.mean(mfccs_diff_norms)
   no_of_frames = len(mfccs_diff_norms)
   return mcd, no_of_frames
@@ -81,4 +112,4 @@ def align_mfccs_with_dtw(mfccs_1: np.ndarray, mfccs_2: np.ndarray) -> Tuple[np.n
   path_for_output = list(map(lambda l: l[1], path_between_mfccs))
   mfccs_1 = mfccs_1[path_for_input]
   mfccs_2 = mfccs_2[path_for_output]
-  return mfccs_1, mfccs_2
+  return mfccs_1.T, mfccs_2.T
